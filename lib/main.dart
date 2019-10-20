7cd1ea4af50606c8c5ec0eb0f53b8920f8e0a1a6
Terrence Curran <tcurran@approachingpi.com>
2019-10-19 20:10:37 -0400

    firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,116 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
+import 'package:flutter_firebase_ui/flutter_firebase_ui.dart';
+import 'package:flutter_firebase_ui/l10n/localization.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return new MaterialApp(
+      title: 'Firebase UI',
+      theme: new ThemeData(
+          primarySwatch: Colors.blue
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        FFULocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'),
+      ],
+      home: new MyHomePage(title: 'Firebase UI'),
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  StreamSubscription<FirebaseUser> _listener;
+  FirebaseUser _currentUser;
+  @override
+  void initState() {
+    super.initState();
+    _checkCurrentUser();
   }
-
+  @override
+  void dispose() {
+    _listener.cancel();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    if (_currentUser == null) {
+      return new SignInScreen(
+        title: 'Firebase UI',
+        header: new Padding(
+          padding: const EdgeInsets.symmetric(vertical: 32.0),
+          child: new Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: new Text("Login"),
+          ),
         ),
+        providers: [
+          ProvidersTypes.google,
+          ProvidersTypes.email
+        ],
+      );
+    } else {
+      return new HomeScreen(user: _currentUser);
+    }
+  }
+  void _checkCurrentUser() async {
+    _currentUser = await _auth.currentUser();
+    _currentUser?.getIdToken(refresh: true);
+    _listener = _auth.onAuthStateChanged.listen((FirebaseUser user) {
+      setState(() {
+        _currentUser = user;
+      });
+    });
+  }
+}
+class HomeScreen extends StatelessWidget {
+  final FirebaseUser user;
+  HomeScreen({this.user});
+  @override
+  Widget build(BuildContext context) => new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Firebase UI'),
+        elevation: 4.0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      body: new Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: new BoxDecoration(color: Colors.amber),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Text("Welcome,"),
+                ],
+              ),
+              new SizedBox(
+                height: 8.0,
+              ),
+              new Text(user.displayName ?? user.email),
+              new SizedBox(
+                height: 32.0,
+              ),
+              new RaisedButton(
+                  child: new Text("Sign Out"), onPressed: _logout)
+            ],
+          )));
+  void _logout() {
+    FirebaseAuth.instance.signOut();
   }
 }
