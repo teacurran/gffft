38f82caa61e7780df110f5db2fb44401ed5abbe9
Terrence Curran <tcurran@approachingpi.com>
2019-12-26 00:48:30 -0500

    trying out a new authentication method

---
+++ lib/src/auth_provider.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:gffft/src/constants.dart';
+
+class AuthProvider {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> sendSignInWithEmailLink(String email) async {
+    return _auth.sendSignInWithEmailLink(
+        email: email,
+        url: Constants.projectUrl,
+        androidInstallIfNotAvailable: true,
+        androidMinimumVersion: '21',
+        androidPackageName: 'com.approachingpi.gffft',
+        handleCodeInApp: true,
+        iOSBundleID: 'com.approachingpi.gffft');
+  }
+
+  Future<AuthResult> signInWithEmailLink(String email, String link) async {
+    return _auth.signInWithEmailAndLink(email: email, link: link);
+  }
+
+  Future<void> verifyPhoneNumber(
+      String phone,
+      PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout,
+      PhoneCodeSent codeSent,
+      Duration duration,
+      PhoneVerificationCompleted verificationCompleted,
+      PhoneVerificationFailed verificationFailed) async {
+    return _auth.verifyPhoneNumber(
+        phoneNumber: phone,
+        codeAutoRetrievalTimeout: codeAutoRetrievalTimeout,
+        codeSent: codeSent,
+        timeout: duration,
+        verificationCompleted: verificationCompleted,
+        verificationFailed: verificationFailed);
+  }
+
+  Future<AuthResult> signInWithCredential(AuthCredential credential) async {
+    return _auth.signInWithCredential(credential);
+  }
+
+  Future<FirebaseUser> getCurrentUser() {
+    return _auth.currentUser();
+  }
+}
