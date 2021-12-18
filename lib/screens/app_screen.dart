1f652b7a0be2cd5b98d01a42a5bf86e982623fd8
Terrence Curran <tea@approachingpi.com>
2021-12-18 18:53:13 -0500

    loading vs loaded main screen

--- lib/screens/app_screen.dart
+++ lib/screens/app_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get_it/get_it.dart';
 import 'package:gffft/src/constants.dart';
 import 'package:gffft/user/user.dart';
@@ -27,14 +28,21 @@ class _AppScreenState extends State<AppScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var i10n = AppLocalizations.of(context);
     return FutureBuilder(
         future: _init(context),
         builder: (context, AsyncSnapshot<User?> snapshot) {
           if (snapshot.hasError) {
-            return Center(child: Text("error loading user info."));
+            return Center(child: Text(i10n!.errorLoading));
           }
-          var data = snapshot.data;
-          String text = Constants.thankYou + ":" + (data != null ? data.id + ":" + data.username : "unknown user");
+          var user = snapshot.data;
+          String text;
+          if (user == null) {
+            text = i10n!.loading;
+          } else {
+            text = Constants.thankYou + ":" + user.username;
+          }
+          String;
           return Center(child: Text(text));
         });
   }
