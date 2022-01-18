172786db4684c59c42ed3cd035ec1ee66f38b41b
Terrence Curran <tea@approachingpi.com>
2022-01-18 01:39:50 -0500

    thread view

--- lib/boards/post_list_item.dart
+++ lib/boards/post_list_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:gffft/common/dates.dart';
 
 import 'models/post.dart';
@@ -19,64 +18,10 @@ class PostListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Slidable(
-        // Specify a key if the Slidable is dismissible.
-        key: const ValueKey(0),
-
-        // The start action pane is the one at the left or the top side.
-        startActionPane: ActionPane(
-          // A motion is a widget used to control how the pane animates.
-          motion: const ScrollMotion(),
-
-          // A pane can dismiss the Slidable.
-          dismissible: DismissiblePane(onDismissed: () {}),
-
-          // All actions are defined in the children parameter.
-          children: [
-            // A SlidableAction can have an icon and/or a label.
-            SlidableAction(
-              onPressed: (context) {},
-              backgroundColor: Color(0xFFFE4A49),
-              foregroundColor: Colors.white,
-              icon: Icons.delete,
-              label: 'Delete',
-            ),
-            SlidableAction(
-              onPressed: (context) {},
-              backgroundColor: Color(0xFF21B7CA),
-              foregroundColor: Colors.white,
-              icon: Icons.share,
-              label: 'Share',
-            ),
-          ],
-        ),
-
-        // The end action pane is the one at the right or the bottom side.
-        endActionPane: ActionPane(
-          motion: ScrollMotion(),
-          children: [
-            SlidableAction(
-              // An action can be bigger than the others.
-              flex: 2,
-              onPressed: (context) {},
-              backgroundColor: Color(0xFF7BC043),
-              foregroundColor: Colors.white,
-              icon: Icons.archive,
-              label: 'Archive',
-            ),
-            SlidableAction(
-              onPressed: (context) {},
-              backgroundColor: Color(0xFF0392CF),
-              foregroundColor: Colors.white,
-              icon: Icons.save,
-              label: 'Save',
-            ),
-          ],
-        ),
+    return Card(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(16, 10, 16, 0),
-          child: Column(
-            children: [
+            padding: EdgeInsets.fromLTRB(16, 10, 16, 0),
+            child: Column(children: [
               Row(
                 children: [
                   Text(
@@ -91,17 +36,17 @@ class PostListItem extends StatelessWidget {
                   ))
                 ],
               ),
-              Row(children: [
-                Expanded(
-                    child: Text(
-                  post.body,
-                  style: theme.textTheme.headline6,
-                  softWrap: true,
-                  textAlign: TextAlign.left,
-                ))
-              ])
-            ],
-          ),
-        ));
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: Row(children: [
+                    Expanded(
+                        child: Text(
+                      post.body,
+                      style: theme.textTheme.headline6,
+                      softWrap: true,
+                      textAlign: TextAlign.left,
+                    ))
+                  ])),
+            ])));
   }
 }
