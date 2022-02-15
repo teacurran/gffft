23293c1fd6cc845411257a0b3a95c2d62c1828f0
Terrence Curran <tea@approachingpi.com>
2022-02-15 10:10:30 -0500

    auto-reload on gallery item after upload

--- lib/galleries/gallery_view_screen.dart
+++ lib/galleries/gallery_view_screen.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get_it/get_it.dart';
+import 'package:gffft/galleries/self_reloading_thumbnail.dart';
 import 'package:gffft/gfffts/models/gffft.dart';
 import 'package:gffft/users/user_api.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-import '../components/common_circular_progress_indicator.dart';
 import 'models/gallery_item.dart';
 
 final getIt = GetIt.instance;
@@ -160,70 +160,16 @@ class _GalleryViewScreenState extends State<GalleryViewScreen> {
                         builderDelegate: PagedChildBuilderDelegate<GalleryItem>(
                           animateTransitions: true,
                           itemBuilder: (context, item, index) {
-                            var thumbUrl = item.urls["320"];
-                            thumbUrl = thumbUrl?.replaceAll("127.0.0.1", storageHost);
+                            Widget thumb = SelfReloadingThumbnail(
+                              uid: widget.uid,
+                              gid: widget.gid,
+                              mid: widget.mid,
+                              iid: item.id,
+                              initialGalleryItem: item,
+                            );
 
                             var fullImageUrl = item.urls["1024"];
                             fullImageUrl = fullImageUrl?.replaceAll("127.0.0.1", storageHost);
-
-                            Widget? thumb;
-                            if (thumbUrl != null && gffft != null && fullImageUrl != null) {
-                              thumb = ExtendedImage.network(thumbUrl, height: 320, width: 320, cache: true,
-                                  loadStateChanged: (ExtendedImageState state) {
-                                Widget? widget;
-                                switch (state.extendedImageLoadState) {
-                                  case LoadState.loading:
-                                    widget = CommonCircularProgressIndicator();
-                                    break;
-                                  case LoadState.completed:
-
-                                    //if you can't konw image size before build,
-                                    //you have to handle crop when image is loaded.
-                                    //so maybe your loading widget size will not the same
-                                    //as image actual size, set returnLoadStateChangedWidget=true,so that
-                                    //image will not to be limited by size which you set for ExtendedImage first time.
-                                    state.returnLoadStateChangedWidget = false;
-
-                                    ///if you don't want override completed widget
-                                    ///please return null or state.completedWidget
-                                    //return null;
-
-                                    thumb = Hero(
-                                        tag: item.id,
-                                        child:
-                                            ExtendedRawImage(image: state.extendedImageInfo?.image, fit: BoxFit.cover));
-
-                                    break;
-                                  case LoadState.failed:
-                                    widget = GestureDetector(
-                                      child: Stack(
-                                        fit: StackFit.expand,
-                                        children: <Widget>[
-                                          Image.asset(
-                                            'assets/failed.jpg',
-                                            fit: BoxFit.fill,
-                                          ),
-                                          const Positioned(
-                                            bottom: 0.0,
-                                            left: 0.0,
-                                            right: 0.0,
-                                            child: Text(
-                                              'load image failed, click to reload',
-                                              textAlign: TextAlign.center,
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                      onTap: () {
-                                        state.reLoadImage();
-                                      },
-                                    );
-                                    break;
-                                }
-                              });
-                            } else {
-                              thumb = CommonCircularProgressIndicator();
-                            }
 
                             return Hero(
                                 tag: item.id,
@@ -284,7 +230,7 @@ class _GalleryViewScreenState extends State<GalleryViewScreen> {
                                                 }));
                                           }
                                         },
-                                        child: thumb!)));
+                                        child: thumb)));
                           },
                         ))
                   ])));
---
+++ lib/galleries/self_reloading_thumbnail.dart
@@ -0,0 +1,160 @@
+import 'dart:async';
+
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:get_it/get_it.dart';
+import 'package:gffft/users/user_api.dart';
+
+import '../components/common_circular_progress_indicator.dart';
+import 'models/gallery_item.dart';
+
+final getIt = GetIt.instance;
+
+class SelfReloadingThumbnail extends StatefulWidget {
+  const SelfReloadingThumbnail({
+    Key? key,
+    required this.uid,
+    required this.gid,
+    required this.mid,
+    required this.iid,
+    this.initialGalleryItem,
+  }) : super(key: key);
+
+  final String uid;
+  final String gid;
+  final String mid;
+  final String iid;
+  final GalleryItem? initialGalleryItem;
+
+  @override
+  State<SelfReloadingThumbnail> createState() => _SelfReloadingThumbnailState();
+}
+
+class _SelfReloadingThumbnailState extends State<SelfReloadingThumbnail> {
+  UserApi userApi = getIt<UserApi>();
+
+  Future<GalleryItem>? galleryItem;
+  final String storageHost = dotenv.get("STORAGE_HOST", fallback: "127.0.0.1");
+
+  @override
+  void initState() {
+    _initialLoadIfNeccessary();
+    super.initState();
+  }
+
+  Future<void> _initialLoadIfNeccessary() async {
+    if (widget.initialGalleryItem != null) {
+      if (widget.initialGalleryItem?.thumbnail ?? false) {
+        var completer = Completer<GalleryItem>();
+        completer.complete(widget.initialGalleryItem);
+        setState(() {
+          galleryItem = completer.future;
+        });
+      }
+    }
+
+    if (galleryItem == null) {
+      var gi = await userApi.getGalleryItem(widget.uid, widget.gid, widget.mid, widget.iid);
+      if (gi.thumbnail) {
+        var completer = Completer<GalleryItem>();
+        completer.complete(gi);
+        setState(() {
+          galleryItem = completer.future;
+        });
+      } else {
+        Timer(const Duration(seconds: 5), () => _initialLoadIfNeccessary);
+      }
+    }
+
+    setState(() {
+      galleryItem = galleryItem;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    AppLocalizations? l10n = AppLocalizations.of(context);
+    final theme = Theme.of(context);
+
+    return FutureBuilder(
+        future: galleryItem,
+        builder: (context, AsyncSnapshot<GalleryItem?> snapshot) {
+          var title = "connecting";
+          if (snapshot.hasError) {
+            title = "error";
+          }
+
+          var item = snapshot.data;
+          if (item == null) {
+            return CommonCircularProgressIndicator();
+          }
+
+          var thumbUrl = item.urls["320"];
+          thumbUrl = thumbUrl?.replaceAll("127.0.0.1", storageHost);
+
+          var fullImageUrl = item.urls["1024"];
+          fullImageUrl = fullImageUrl?.replaceAll("127.0.0.1", storageHost);
+
+          Widget? thumb;
+          if (thumbUrl != null && fullImageUrl != null) {
+            thumb = ExtendedImage.network(thumbUrl, height: 320, width: 320, cache: true,
+                loadStateChanged: (ExtendedImageState state) {
+              Widget? widget;
+              switch (state.extendedImageLoadState) {
+                case LoadState.loading:
+                  widget = CommonCircularProgressIndicator();
+                  break;
+                case LoadState.completed:
+
+                  //if you can't konw image size before build,
+                  //you have to handle crop when image is loaded.
+                  //so maybe your loading widget size will not the same
+                  //as image actual size, set returnLoadStateChangedWidget=true,so that
+                  //image will not to be limited by size which you set for ExtendedImage first time.
+                  state.returnLoadStateChangedWidget = false;
+
+                  ///if you don't want override completed widget
+                  ///please return null or state.completedWidget
+                  //return null;
+
+                  thumb = Hero(
+                      tag: item.id, child: ExtendedRawImage(image: state.extendedImageInfo?.image, fit: BoxFit.cover));
+
+                  break;
+                case LoadState.failed:
+                  widget = GestureDetector(
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: <Widget>[
+                        Image.asset(
+                          'assets/failed.jpg',
+                          fit: BoxFit.fill,
+                        ),
+                        const Positioned(
+                          bottom: 0.0,
+                          left: 0.0,
+                          right: 0.0,
+                          child: Text(
+                            'load image failed, click to reload',
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                    ),
+                    onTap: () {
+                      state.reLoadImage();
+                    },
+                  );
+                  break;
+              }
+            });
+          }
+
+          thumb ??= CommonCircularProgressIndicator();
+
+          return thumb!;
+        });
+  }
+}
