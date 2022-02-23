dac30dc63ab79c5684b24245cc42685e17fb0574
Terrence Curran <tea@approachingpi.com>
2022-02-23 00:27:48 -0500

    opening gffft in tab

--- lib/home/tabbed_home_screen.dart
+++ lib/home/tabbed_home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart' as fbAuth;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get_it/get_it.dart';
 import 'package:gffft/components/first_page_exception_indicator.dart';
@@ -38,6 +39,9 @@ class _TabbedHomeScreenState extends State<TabbedHomeScreen> with SingleTickerPr
 
   static const _pageSize = 100;
   String? _searchTerm;
+  int _selectedIndex = 0;
+  List<BuildContext?> navStack = [null, null, null]; // one buildContext for each tab to store history  of navigation
+  late List<Widget> mainTabs;
 
   final PagingController<String?, Bookmark> _bookmarkController = PagingController(firstPageKey: null);
   final PagingController<String?, GffftMinimal> _searchController = PagingController(firstPageKey: null);
@@ -96,6 +100,35 @@ class _TabbedHomeScreenState extends State<TabbedHomeScreen> with SingleTickerPr
     });
 
     _tabController = TabController(length: 3, vsync: this);
+
+    mainTabs = <Widget>[
+      Navigator(onGenerateRoute: (RouteSettings settings) {
+        return PageRouteBuilder(pageBuilder: (context, animiX, animiY) {
+          // use page PageRouteBuilder instead of 'PageRouteBuilder' to avoid material route animation
+          var l10n = AppLocalizations.of(context);
+          final theme = Theme.of(context);
+
+          navStack[0] = context;
+          return _getSearchScreen(theme, l10n);
+        });
+      }),
+      Navigator(onGenerateRoute: (RouteSettings settings) {
+        return PageRouteBuilder(pageBuilder: (context, animiX, animiY) {
+          // use page PageRouteBuilder instead of 'PageRouteBuilder' to avoid material route animation
+
+          navStack[1] = context;
+          return GffftHomeScreen(uid: "me", gid: "default");
+        });
+      }),
+      Navigator(onGenerateRoute: (RouteSettings settings) {
+        return PageRouteBuilder(pageBuilder: (context, animiX, animiY) {
+          // use page PageRouteBuilder instead of 'PageRouteBuilder' to avoid material route animation
+
+          navStack[1] = context;
+          return MeScreen();
+        });
+      }),
+    ];
 
     _loadData();
 
@@ -159,33 +192,54 @@ class _TabbedHomeScreenState extends State<TabbedHomeScreen> with SingleTickerPr
     final theme = Theme.of(context);
 
     return Theme(
-      data: ThemeData.dark().copyWith(
-          appBarTheme: theme.appBarTheme,
-          scaffoldBackgroundColor: theme.scaffoldBackgroundColor,
-          textTheme: theme.textTheme),
-      child: Scaffold(
-        appBar: AppBar(
-          flexibleSpace: SafeArea(
-              child: TabBar(
-            controller: _tabController,
-            tabs: [
-              Tab(icon: Icon(Icons.home)),
-              Tab(icon: Icon(IconData(0x0047, fontFamily: 'Gffft'))),
-              Tab(icon: Icon(Icons.account_box)),
-            ],
-          )),
-          automaticallyImplyLeading: false,
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: [
-            _getSearchScreen(theme, l10n),
-            const GffftHomeScreen(uid: "me", gid: "default"),
-            MeScreen(),
-          ],
-        ),
-      ),
-    );
+        data: ThemeData.dark().copyWith(
+            appBarTheme: theme.appBarTheme,
+            scaffoldBackgroundColor: theme.scaffoldBackgroundColor,
+            textTheme: theme.textTheme),
+        child: WillPopScope(
+          onWillPop: () async {
+            var whatContext = navStack[_tabController.index] ?? context;
+            if (Navigator.of(whatContext).canPop()) {
+              Navigator.of(whatContext).pop();
+              setState(() {
+                _selectedIndex = _tabController.index;
+              });
+              return false;
+            } else {
+              if (_tabController.index == 0) {
+                setState(() {
+                  _selectedIndex = _tabController.index;
+                });
+                SystemChannels.platform.invokeMethod('SystemNavigator.pop'); // close the app
+                return true;
+              } else {
+                _tabController.index = 0; // back to first tap if current tab history stack is empty
+                setState(() {
+                  _selectedIndex = _tabController.index;
+                });
+                return false;
+              }
+            }
+          },
+          child: Scaffold(
+            appBar: AppBar(
+              flexibleSpace: SafeArea(
+                  child: TabBar(
+                controller: _tabController,
+                tabs: [
+                  Tab(icon: Icon(Icons.home)),
+                  Tab(icon: Icon(IconData(0x0047, fontFamily: 'Gffft'))),
+                  Tab(icon: Icon(Icons.account_box)),
+                ],
+              )),
+              automaticallyImplyLeading: false,
+            ),
+            body: TabBarView(
+              controller: _tabController,
+              children: mainTabs,
+            ),
+          ),
+        ));
   }
 
   Widget _getFeaturedScreen(theme, l10n) {
