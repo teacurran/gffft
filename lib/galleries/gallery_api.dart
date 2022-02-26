5de7f979f70f2f6d67f72acc9e25541d2638f815
Terrence Curran <tea@approachingpi.com>
2022-02-26 11:54:33 -0500

    reverting streamed image upload

--- lib/galleries/gallery_api.dart
+++ lib/galleries/gallery_api.dart
@@ -8,16 +8,8 @@ import 'package:image_picker/image_picker.dart';
 
 import '../api_base.dart';
 
-typedef void OnUploadProgressCallback(int sentBytes, int totalBytes);
-
 class GalleryApi extends ApiBase {
-  static HttpClient getHttpClient() {
-    HttpClient httpClient = HttpClient()..connectionTimeout = const Duration(seconds: 120);
-    return httpClient;
-  }
-
-  Future<void> uploadGalleryItem(String uid, String gid, String mid, String description, XFile file,
-      {OnUploadProgressCallback? onUploadProgress}) async {
+  Future<void> uploadGalleryItem(String uid, String gid, String mid, String description, XFile file) async {
     FirebaseAuth fbAuth = FirebaseAuth.instance;
 
     var url = Uri.parse(getBaseUrl() + "galleries");
@@ -25,10 +17,9 @@ class GalleryApi extends ApiBase {
       print('Posting gallery item, url $url');
     }
 
-    String? responseJson;
+    dynamic responseJson;
     try {
-      final httpClient = getHttpClient();
-      final request = await httpClient.postUrl(url);
+      final request = http.MultipartRequest("POST", url);
 
       // this method always requires auth
       if (fbAuth.currentUser == null) {
@@ -38,62 +29,24 @@ class GalleryApi extends ApiBase {
         if (kDebugMode) {
           print('auth token: $token');
         }
-        request.headers.set("Authorization", "Bearer $token");
+        request.headers.addAll({
+          "Authorization": "Bearer $token",
+        });
       }
-
-      final requestMultipart = http.MultipartRequest("POST", url);
 
       var image = http.MultipartFile.fromBytes('file', await file.readAsBytes(), filename: file.name);
-      requestMultipart.files.add(image);
-      requestMultipart.fields['uid'] = uid;
-      requestMultipart.fields['gid'] = gid;
-      requestMultipart.fields['mid'] = mid;
-      requestMultipart.fields['description'] = description;
+      request.files.add(image);
+      request.fields['uid'] = uid;
+      request.fields['gid'] = gid;
+      request.fields['mid'] = mid;
+      request.fields['description'] = description;
 
-      var msStream = requestMultipart.finalize();
-      var totalByteLength = requestMultipart.contentLength;
-
-      var multipartContentType = requestMultipart.headers[HttpHeaders.contentTypeHeader];
-      if (multipartContentType != null) {
-        request.headers.set(HttpHeaders.contentTypeHeader, multipartContentType);
-      }
-
-      request.contentLength = totalByteLength;
-      int byteCount = 0;
-      Stream<List<int>> streamUpload = msStream.transform(
-        StreamTransformer.fromHandlers(
-          handleData: (data, sink) {
-            byteCount += data.length;
-
-            if (onUploadProgress != null) {
-              onUploadProgress(byteCount, totalByteLength);
-            }
-
-            sink.add(data);
-          },
-          handleError: (error, stack, sink) {
-            print(error.toString());
-          },
-          handleDone: (sink) {
-            sink.close();
-            // UPLOAD DONE;
-          },
-        ),
-      );
-
-      // var image = http.MultipartFile.fromBytes('file', await file.readAsBytes(), filename: file.name);
-
-      await request.addStream(streamUpload);
-
-      //final streamedResponse = await request.send();
-      // var response = await http.Response.fromStream(streamedResponse);
-
-      final response = await request.close();
-
+      final streamedResponse = await request.send();
+      var response = await http.Response.fromStream(streamedResponse);
       if (response.statusCode == 403) {
         await fbAuth.signOut();
       }
-      //responseJson = await returnResponseFromClient(response);
+      responseJson = returnResponse(response);
     } catch (e, stacktrace) {
       if (e is SocketException) {
         //treat SocketException
@@ -115,6 +68,6 @@ class GalleryApi extends ApiBase {
 
       throw FetchDataException('Unable to post gallery item');
     }
-    //return responseJson;
+    return responseJson;
   }
 }
