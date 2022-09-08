8f9e1299df9c87d7c8aa3227b61fc247411c0d8d
Terrence Curran <tcurran@approachingpi.com>
2022-09-08 00:59:25 -0400

    updated packages in pubspec

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -40,141 +40,196 @@ class MessageLookup extends MessageLookupByLibrary {
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "boardCreatePostBody": MessageLookupByLibrary.simpleMessage("body"),
         "boardCreatePostPost": MessageLookupByLibrary.simpleMessage("post!"),
-        "boardCreatePostSubject": MessageLookupByLibrary.simpleMessage("subject"),
+        "boardCreatePostSubject":
+            MessageLookupByLibrary.simpleMessage("subject"),
         "boardCreatePostTitle": MessageLookupByLibrary.simpleMessage("post"),
         "boardReplyToThread": MessageLookupByLibrary.simpleMessage("reply"),
-        "boardThreadAuthorReport": MessageLookupByLibrary.simpleMessage("report"),
-        "boardThreadAuthorVisit": MessageLookupByLibrary.simpleMessage("profile"),
-        "boardViewActionTooltip": MessageLookupByLibrary.simpleMessage("add a post"),
+        "boardThreadAuthorReport":
+            MessageLookupByLibrary.simpleMessage("report"),
+        "boardThreadAuthorVisit":
+            MessageLookupByLibrary.simpleMessage("profile"),
+        "boardViewActionTooltip":
+            MessageLookupByLibrary.simpleMessage("add a post"),
         "bookmarks": MessageLookupByLibrary.simpleMessage("bookmarks"),
-        "changeUsername": MessageLookupByLibrary.simpleMessage("Change Username"),
+        "changeUsername":
+            MessageLookupByLibrary.simpleMessage("Change Username"),
         "connect": MessageLookupByLibrary.simpleMessage("connect"),
         "editActions": MessageLookupByLibrary.simpleMessage("Actions"),
-        "editAllowMembers": MessageLookupByLibrary.simpleMessage("Allow Members?"),
+        "editAllowMembers":
+            MessageLookupByLibrary.simpleMessage("Allow Members?"),
         "editBoard": MessageLookupByLibrary.simpleMessage("Board"),
-        "editBoardWhoCanPost": MessageLookupByLibrary.simpleMessage("Who can post?"),
-        "editBoardWhoCanView": MessageLookupByLibrary.simpleMessage("Who can view?"),
+        "editBoardWhoCanPost":
+            MessageLookupByLibrary.simpleMessage("Who can post?"),
+        "editBoardWhoCanView":
+            MessageLookupByLibrary.simpleMessage("Who can view?"),
         "editBoards": MessageLookupByLibrary.simpleMessage("Boards"),
         "editCalendar": MessageLookupByLibrary.simpleMessage("Calendar"),
         "editDescription": MessageLookupByLibrary.simpleMessage("Description"),
         "editDescriptionHint": MessageLookupByLibrary.simpleMessage(
             "A short description of your gffft that will show up in search results"),
         "editEnable": MessageLookupByLibrary.simpleMessage("Enable"),
-        "editEnableAltHandles": MessageLookupByLibrary.simpleMessage("Enable alternate handles"),
-        "editEnableAltHandlesHint":
-            MessageLookupByLibrary.simpleMessage("Allows members of your gffft to choose usernames"),
-        "editEnableMembership": MessageLookupByLibrary.simpleMessage("Enable membership"),
+        "editEnableAltHandles":
+            MessageLookupByLibrary.simpleMessage("Enable alternate handles"),
+        "editEnableAltHandlesHint": MessageLookupByLibrary.simpleMessage(
+            "Allows members of your gffft to choose usernames"),
+        "editEnableMembership":
+            MessageLookupByLibrary.simpleMessage("Enable membership"),
         "editEnabled": MessageLookupByLibrary.simpleMessage("Enabled"),
         "editGalleries": MessageLookupByLibrary.simpleMessage("Galleries"),
         "editGallery": MessageLookupByLibrary.simpleMessage("Gallery"),
         "editIntro": MessageLookupByLibrary.simpleMessage("Introduction"),
-        "editIntroHint": MessageLookupByLibrary.simpleMessage("Longer introduction to this gffft"),
+        "editIntroHint": MessageLookupByLibrary.simpleMessage(
+            "Longer introduction to this gffft"),
         "editMembership": MessageLookupByLibrary.simpleMessage("Membership"),
         "editName": MessageLookupByLibrary.simpleMessage("Name"),
-        "editRequireApproval": MessageLookupByLibrary.simpleMessage("Require Approval?"),
-        "editRequireApprovalHint":
-            MessageLookupByLibrary.simpleMessage("People will have to apply for access to your gffft."),
+        "editNotebook": MessageLookupByLibrary.simpleMessage("Notebook"),
+        "editRequireApproval":
+            MessageLookupByLibrary.simpleMessage("Require Approval?"),
+        "editRequireApprovalHint": MessageLookupByLibrary.simpleMessage(
+            "People will have to apply for access to your gffft."),
         "editSave": MessageLookupByLibrary.simpleMessage("Save"),
         "editTags": MessageLookupByLibrary.simpleMessage("Tags"),
-        "editTagsHint": MessageLookupByLibrary.simpleMessage("Tags allow people to find your gffft when searching"),
+        "editTagsHint": MessageLookupByLibrary.simpleMessage(
+            "Tags allow people to find your gffft when searching"),
         "editTagsInputHint": MessageLookupByLibrary.simpleMessage("Add a #tag"),
-        "editYourGffftInfo": MessageLookupByLibrary.simpleMessage("Your Gffft Info"),
+        "editYourGffftInfo":
+            MessageLookupByLibrary.simpleMessage("Your Gffft Info"),
         "errorLoading": MessageLookupByLibrary.simpleMessage("Error Loading"),
-        "errorYouMustBeAMember": MessageLookupByLibrary.simpleMessage("You must be a member for that"),
-        "errorYouMustBeSignedInForThat": MessageLookupByLibrary.simpleMessage("You must be signed in for that"),
+        "errorYouMustBeAMember": MessageLookupByLibrary.simpleMessage(
+            "You must be a member for that"),
+        "errorYouMustBeSignedInForThat": MessageLookupByLibrary.simpleMessage(
+            "You must be signed in for that"),
         "flag": MessageLookupByLibrary.simpleMessage("Flag"),
-        "galleryItemDescription": MessageLookupByLibrary.simpleMessage("Description"),
-        "galleryPostCaption": MessageLookupByLibrary.simpleMessage("Description, if you feel like it..."),
+        "galleryItemDescription":
+            MessageLookupByLibrary.simpleMessage("Description"),
+        "galleryPostCaption": MessageLookupByLibrary.simpleMessage(
+            "Description, if you feel like it..."),
         "galleryPostTitle": MessageLookupByLibrary.simpleMessage("post"),
-        "gffftCreateCreateButton": MessageLookupByLibrary.simpleMessage("create"),
-        "gffftCreateHead": MessageLookupByLibrary.simpleMessage("create a gffft"),
-        "gffftCreateIntro": MessageLookupByLibrary.simpleMessage("To create a gffft, first give it a name:"),
+        "gffftCreateCreateButton":
+            MessageLookupByLibrary.simpleMessage("create"),
+        "gffftCreateHead":
+            MessageLookupByLibrary.simpleMessage("create a gffft"),
+        "gffftCreateIntro": MessageLookupByLibrary.simpleMessage(
+            "To create a gffft, first give it a name:"),
         "gffftCreateNextButton": MessageLookupByLibrary.simpleMessage("next"),
         "gffftEdit": MessageLookupByLibrary.simpleMessage("edit"),
-        "gffftEditCardDescription": MessageLookupByLibrary.simpleMessage("description (optional)"),
-        "gffftEditCardIntro": MessageLookupByLibrary.simpleMessage("intro (optional)"),
+        "gffftEditCardDescription":
+            MessageLookupByLibrary.simpleMessage("description (optional)"),
+        "gffftEditCardIntro":
+            MessageLookupByLibrary.simpleMessage("intro (optional)"),
         "gffftEditCardName": MessageLookupByLibrary.simpleMessage("name"),
         "gffftEditStop": MessageLookupByLibrary.simpleMessage("stop editing"),
         "gffftHomeBlog": MessageLookupByLibrary.simpleMessage("blog"),
         "gffftHomeBoard": MessageLookupByLibrary.simpleMessage("board"),
         "gffftHomeBoardPosts": MessageLookupByLibrary.simpleMessage("posts:"),
         "gffftHomeBoardPostsNew": m0,
-        "gffftHomeBoardThreads": MessageLookupByLibrary.simpleMessage("threads:"),
+        "gffftHomeBoardThreads":
+            MessageLookupByLibrary.simpleMessage("threads:"),
         "gffftHomeBoardThreadsNew": m1,
         "gffftHomeBookmark": MessageLookupByLibrary.simpleMessage("bookmark"),
         "gffftHomeCalendar": MessageLookupByLibrary.simpleMessage("calendar"),
         "gffftHomeGallery": MessageLookupByLibrary.simpleMessage("gallery"),
-        "gffftHomeGalleryPhotos": MessageLookupByLibrary.simpleMessage("photos:"),
+        "gffftHomeGalleryPhotos":
+            MessageLookupByLibrary.simpleMessage("photos:"),
         "gffftHomeGalleryPhotosNew": m2,
-        "gffftHomeGalleryVideos": MessageLookupByLibrary.simpleMessage("videos:"),
+        "gffftHomeGalleryVideos":
+            MessageLookupByLibrary.simpleMessage("videos:"),
         "gffftHomeGalleryVideosNew": m3,
         "gffftHomeJoin": MessageLookupByLibrary.simpleMessage("join"),
         "gffftHomeLinkSet": MessageLookupByLibrary.simpleMessage("links"),
         "gffftHomeLinkSetLinks": MessageLookupByLibrary.simpleMessage("links:"),
         "gffftHomeLinkSetLinksNew": m4,
         "gffftHomeMemberSince": m5,
-        "gffftHomeMembership": MessageLookupByLibrary.simpleMessage("membership"),
-        "gffftHomeNotMember": MessageLookupByLibrary.simpleMessage("not a member"),
+        "gffftHomeMembership":
+            MessageLookupByLibrary.simpleMessage("membership"),
+        "gffftHomeNotMember":
+            MessageLookupByLibrary.simpleMessage("not a member"),
         "gffftHomePages": MessageLookupByLibrary.simpleMessage("notebook"),
         "gffftHomeQuit": MessageLookupByLibrary.simpleMessage("quit"),
-        "gffftHomeUnBookmark": MessageLookupByLibrary.simpleMessage("un-bookmark"),
+        "gffftHomeUnBookmark":
+            MessageLookupByLibrary.simpleMessage("un-bookmark"),
         "gffftIntro": MessageLookupByLibrary.simpleMessage(
             "This is your gffft. A space where you can create and share content with others.  You can keep your gffft private, make it public, or invite others to join you. To get started, edit this text or add a feature below."),
         "gffftJoin": MessageLookupByLibrary.simpleMessage("join this gffft"),
         "gffftJoinButton": MessageLookupByLibrary.simpleMessage("join!"),
         "gffftJoinHandle": MessageLookupByLibrary.simpleMessage("handle"),
-        "gffftJoinHandleError":
-            MessageLookupByLibrary.simpleMessage("Sorry, that handle is already taken on this gffft."),
-        "gffftJoinHandleHint":
-            MessageLookupByLibrary.simpleMessage("What would you like to be known as on this gffft?"),
+        "gffftJoinHandleError": MessageLookupByLibrary.simpleMessage(
+            "Sorry, that handle is already taken on this gffft."),
+        "gffftJoinHandleHint": MessageLookupByLibrary.simpleMessage(
+            "What would you like to be known as on this gffft?"),
         "gffftJoinRules": MessageLookupByLibrary.simpleMessage("rules"),
-        "gffftListSearchHint": MessageLookupByLibrary.simpleMessage("name or fruit-code"),
-        "gffftMembershipSaveButton": MessageLookupByLibrary.simpleMessage("save!"),
-        "gffftMembershipTitle": MessageLookupByLibrary.simpleMessage("your membership"),
-        "gffftSettingsBoardWhoCanPost": MessageLookupByLibrary.simpleMessage("Who can post:"),
-        "gffftSettingsBoardWhoCanView": MessageLookupByLibrary.simpleMessage("Who can view:"),
-        "gffftSettingsEnableCalendar": MessageLookupByLibrary.simpleMessage("Enable calendar"),
+        "gffftListSearchHint":
+            MessageLookupByLibrary.simpleMessage("name or fruit-code"),
+        "gffftMembershipSaveButton":
+            MessageLookupByLibrary.simpleMessage("save!"),
+        "gffftMembershipTitle":
+            MessageLookupByLibrary.simpleMessage("your membership"),
+        "gffftSettingsBoardWhoCanPost":
+            MessageLookupByLibrary.simpleMessage("Who can post:"),
+        "gffftSettingsBoardWhoCanView":
+            MessageLookupByLibrary.simpleMessage("Who can view:"),
+        "gffftSettingsEnableCalendar":
+            MessageLookupByLibrary.simpleMessage("Enable calendar"),
         "gffftSettingsEnableCalendarHint": MessageLookupByLibrary.simpleMessage(
             "A shared calendar that is designed for shared events in the future as well as memories in the past. This is not your office calendar."),
-        "gffftSettingsEnableMembership": MessageLookupByLibrary.simpleMessage("Enable membership"),
+        "gffftSettingsEnableMembership":
+            MessageLookupByLibrary.simpleMessage("Enable membership"),
         "gffftSettingsEnableMembershipHint": MessageLookupByLibrary.simpleMessage(
             "This allows other people to join your gffft and participate in message boards, galleries, or other features.  Be careful before enabling membership."),
-        "gffftSettingsEnableMessageBoard": MessageLookupByLibrary.simpleMessage("Enable Message Board"),
+        "gffftSettingsEnableMessageBoard":
+            MessageLookupByLibrary.simpleMessage("Enable Message Board"),
         "gffftSettingsEnableMessageBoardHint": MessageLookupByLibrary.simpleMessage(
             "A message board enables you or your guests to post text based threaded messages."),
-        "gffftSettingsEnabled": MessageLookupByLibrary.simpleMessage("Show in search"),
+        "gffftSettingsEnabled":
+            MessageLookupByLibrary.simpleMessage("Show in search"),
         "gffftSettingsEnabledHint": MessageLookupByLibrary.simpleMessage(
             "This will enable your gffft to be searched.  To share with just your friends, use a fruit-code instead."),
-        "gffftSettingsFruitCode": MessageLookupByLibrary.simpleMessage("fruit code"),
-        "gffftSettingsFruitCodeCopied": MessageLookupByLibrary.simpleMessage("fruit code copied to the clipboard"),
-        "gffftSettingsFruitCodeCopy": MessageLookupByLibrary.simpleMessage("copy to clipboard"),
-        "gffftSettingsFruitCodeEnable": MessageLookupByLibrary.simpleMessage("show fruit code"),
-        "gffftSettingsFruitCodeHint":
-            MessageLookupByLibrary.simpleMessage("use fruit codes to share your gffft with others"),
-        "gffftSettingsFruitCodeShare": MessageLookupByLibrary.simpleMessage("¡gffft!"),
-        "gffftSettingsGalleryEnable": MessageLookupByLibrary.simpleMessage("Enable gallery"),
+        "gffftSettingsFruitCode":
+            MessageLookupByLibrary.simpleMessage("fruit code"),
+        "gffftSettingsFruitCodeCopied": MessageLookupByLibrary.simpleMessage(
+            "fruit code copied to the clipboard"),
+        "gffftSettingsFruitCodeCopy":
+            MessageLookupByLibrary.simpleMessage("copy to clipboard"),
+        "gffftSettingsFruitCodeEnable":
+            MessageLookupByLibrary.simpleMessage("show fruit code"),
+        "gffftSettingsFruitCodeHint": MessageLookupByLibrary.simpleMessage(
+            "use fruit codes to share your gffft with others"),
+        "gffftSettingsFruitCodeShare":
+            MessageLookupByLibrary.simpleMessage("¡gffft!"),
+        "gffftSettingsGalleryEnable":
+            MessageLookupByLibrary.simpleMessage("Enable gallery"),
         "gffftSettingsGalleryEnableHint": MessageLookupByLibrary.simpleMessage(
             "A gallery allows you or your gffft\'s members to post photos and videos. Configure to be viewed as a stream or contact-sheet style."),
-        "gffftSettingsGeneralHead": MessageLookupByLibrary.simpleMessage("general"),
+        "gffftSettingsGeneralHead":
+            MessageLookupByLibrary.simpleMessage("general"),
         "gffftSettingsHead": MessageLookupByLibrary.simpleMessage("settings"),
-        "gffftSettingsLinkSetEnable": MessageLookupByLibrary.simpleMessage("Enable links"),
-        "gffftSettingsLinkSetEnableHint": MessageLookupByLibrary.simpleMessage("Save links for yourself or others"),
-        "gffftSettingsNotebookEnable": MessageLookupByLibrary.simpleMessage("Enable notebook"),
+        "gffftSettingsLinkSetEnable":
+            MessageLookupByLibrary.simpleMessage("Enable links"),
+        "gffftSettingsLinkSetEnableHint": MessageLookupByLibrary.simpleMessage(
+            "Save links for yourself or others"),
+        "gffftSettingsNotebookEnable":
+            MessageLookupByLibrary.simpleMessage("Enable notebook"),
         "gffftSettingsNotebookEnableHint": MessageLookupByLibrary.simpleMessage(
             "Notebooks can be used to create pages to share. Notebooks can be configured to display content inline like a blog, or as a list."),
-        "gffftSettingsNotebookWhoCanEdit": MessageLookupByLibrary.simpleMessage("Who can edit:"),
-        "gffftSettingsNotebookWhoCanView": MessageLookupByLibrary.simpleMessage("Who can view:"),
-        "gffftSettingsSave": MessageLookupByLibrary.simpleMessage("save and exit"),
+        "gffftSettingsNotebookWhoCanEdit":
+            MessageLookupByLibrary.simpleMessage("Who can edit:"),
+        "gffftSettingsNotebookWhoCanView":
+            MessageLookupByLibrary.simpleMessage("Who can view:"),
+        "gffftSettingsSave":
+            MessageLookupByLibrary.simpleMessage("save and exit"),
         "homeAccountSince": m6,
         "host": MessageLookupByLibrary.simpleMessage("Host"),
-        "linkSetPostDescription": MessageLookupByLibrary.simpleMessage("Description (optional)"),
+        "linkSetPostDescription":
+            MessageLookupByLibrary.simpleMessage("Description (optional)"),
         "linkSetPostUrl": MessageLookupByLibrary.simpleMessage("https://"),
-        "linkSetViewActionTooltip": MessageLookupByLibrary.simpleMessage("Add a Link"),
+        "linkSetViewActionTooltip":
+            MessageLookupByLibrary.simpleMessage("Add a Link"),
         "loading": MessageLookupByLibrary.simpleMessage("Loading"),
         "loggedIn": MessageLookupByLibrary.simpleMessage("logged in"),
         "logout": MessageLookupByLibrary.simpleMessage("Logout"),
         "me": MessageLookupByLibrary.simpleMessage("me"),
-        "memberTypeAdmin": MessageLookupByLibrary.simpleMessage("Administrator"),
+        "memberTypeAdmin":
+            MessageLookupByLibrary.simpleMessage("Administrator"),
         "memberTypeAnon": MessageLookupByLibrary.simpleMessage("Anonymous"),
         "memberTypeMember": MessageLookupByLibrary.simpleMessage("Member"),
         "memberTypeSysop": MessageLookupByLibrary.simpleMessage("Sysop"),
